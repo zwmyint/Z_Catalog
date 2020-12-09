4d50bcc2fdf896d67eda8bb301465e285f9fe6a8
ZAW WIN MYINT <45326766+zwmyint@users.noreply.github.com>
2020-12-09 23:15:17 +0800

    first commit

---
+++ Dtos/ItemDto.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Catalog.Dtos
+{
+    public record ItemDto
+    {
+        
+        public Guid Id {get; init;}
+
+        public string Name {get; init;}
+
+        public decimal Price {get; init;}
+
+        public string NewData {get; init;}
+
+        public DateTimeOffset CreatedDate {get; init;}
+    }
+}
